e73d211ac47c28bd303130c814f7a10309e678ce
linh0511 <linh0511@users.noreply.github.com>
2019-07-15 00:22:05 +0700

    Props

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -3,11 +3,53 @@ import Header from './components/Header';
 import Product from './components/Product';
 
 class App extends Component {
+
+
   render() {
+    var products = [
+      {
+        id: 1,
+        name: 'Iphone',
+        price: 100000,
+        image: "https://cdn.tgdd.vn/Products/Images/42/190323/iphone-xs-gold-600x600.jpg",
+        status: true
+      },
+      {
+        id: 2,
+        name: 'Samsung',
+        price: 120000,
+        image: "https://cdn.tgdd.vn/Products/Images/42/179530/samsung-galaxy-s10-plus-black-600x600.jpg",
+        status: true
+      },
+      {
+        id: 3,
+        name: 'Oppo',
+        price: 150000,
+        image: "https://cdn.tgdd.vn/Products/Images/42/182153/oppo-f9-tim-600x600.jpg",
+        status: true
+      }
+    ];
+
+    let elements = products.map((product, index) => {
+      if (product.status) {
+        return (
+          <Product
+            key={product.id}
+            price={product.price}
+            image={product.image}
+          >
+            {product.name}
+          </Product>
+        )
+      }
+    });
+
     return (
       <div className="container">
         <Header />
-        <Product />
+        <div className="row my-5">
+          {elements}
+        </div>
       </div>
     )
   }
--- lesson1/src/components/Product.js
+++ lesson1/src/components/Product.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
+    addToCart = () => {
+        alert('Đặt mua thành công ' + this.props.children);
+    }
     render() {
         return (
-            <div className="row mt-5">
-                <div className="col-xs-6 col-sm-6 col-md-4">
-                    <div className="card">
-                        <img className="card-img-top" src="https://cdn.tgdd.vn/Products/Images/42/190323/iphone-xs-gold-400x460.png" alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">Iphone XS Max</h5>
-                            <p className="card-text"> 12.000.000 VND</p>
-                            <a href="#" className="btn btn-primary">Đặt Mua</a>
-                        </div>
+            <div className="col-xs-6 col-sm-6 col-md-4">
+                <div className="card">
+                    <img className="card-img-top py-3" src={this.props.image} alt={this.props.children} />
+                    <div className="card-body">
+                        <h5 className="card-title"> {this.props.children} </h5>
+                        <p className="card-text"> {this.props.price} VND</p>
+                        <a href="#" className="btn btn-primary" onClick={this.addToCart} >Đặt Mua</a>
                     </div>
                 </div>
             </div>
